9e96b78c60d09d8c9cf143a4e91788164d9f6f92
Derrick Ho <derrickho328@derricks-macbook-pro.local>
2018-01-22 12:22:22 -0800

    Initial Commit

---
+++ ScreenAnnotation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScreenAnnotation
+//
+//  Created by Derrick Ho on 1/22/18.
+//  Copyright © 2018 Derrick Ho. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
